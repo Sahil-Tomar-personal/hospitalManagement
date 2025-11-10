f6b681f3746f352eed2b19f79ec43b3db6b0a750
Sahil Tomar <stomar21@iitk.ac.in>
2025-11-10 18:24:02 +0530

    First Commit

---
+++ src/test/java/com/tomar/sahil/hospitalManagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomar.sahil.hospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
